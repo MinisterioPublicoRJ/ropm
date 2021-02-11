fc88103d6a98c2458c8d07b6c2912467a4e01902
rhenanbartels <rhenan.bartels@gmail.com>
2021-02-11 16:43:54 -0300

    Atualiza informações do admin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
 
 from users.models import User
 
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("instituicao",)
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "instituicao")}),
+        (_("Permissions"), {
+            "fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions"),
+        }),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
