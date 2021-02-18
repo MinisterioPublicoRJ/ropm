b185296a3e8be5a9289844f4d45546ff3aaa0196
rhenanbartels <rhenan.bartels@gmail.com>
2021-02-18 09:28:40 -0300

    Adiciona rota para view de logout

--- conf/urls.py
+++ conf/urls.py
@@ -23,6 +23,7 @@ API_VERSION = "v1"
 
 urlpatterns = [
     path("conta/login", auth_views.LoginView.as_view(), name="login"),
+    path("conta/logout", auth_views.LogoutView.as_view(), name="logout"),
     path('admin/', admin.site.urls),
     path("operacoes/", include("operations.urls", namespace="operations")),
     path(f"{API_VERSION}/operacoes/", include("operations.api_urls", namespace="api-operations")),
