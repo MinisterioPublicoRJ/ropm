793151b03c9dd778e554aefc01bf729207d0dd08
rhenanbartels <rhenan.bartels@gmail.com>
2021-02-19 09:11:30 -0300

    Configura rota para API de bpm e delegacias

--- coredata/api_views.py
+++ coredata/api_views.py
@@ -1,8 +1,18 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
-from coredata.models import Bairro, Municipio
-from coredata.serializers import BairroSerializer, MunicipioSerializer
+from coredata.models import (
+    Bairro,
+    Batalhao,
+    Delegacia,
+    Municipio,
+)
+from coredata.serializers import (
+    BairroSerializer,
+    BatalhaoSerializer,
+    DelegaciaSerializer,
+    MunicipioSerializer
+)
 
 
 class MunicipiosView(ListAPIView):
@@ -21,3 +31,27 @@ class BairrosView(ListAPIView):
         return Bairro.objects.filter(
             cod_mun__cod_6_dig=cod_mun
         ).order_by("bairro")
+
+
+class BatalhaoView(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = BatalhaoSerializer
+    lookup_url_kwarg = "cod_mun"
+
+    def get_queryset(self):
+        cod_mun = self.kwargs.get(self.lookup_url_kwarg)
+        return Batalhao.objects.filter(
+            codigo_mun__cod_6_dig=cod_mun
+        ).order_by("bpm").distinct("bpm")
+
+
+class DelegaciaView(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = DelegaciaSerializer
+    lookup_url_kwarg = "cod_mun"
+
+    def get_queryset(self):
+        cod_mun = self.kwargs.get(self.lookup_url_kwarg)
+        return Delegacia.objects.filter(
+            cod_mun__cod_6_dig=cod_mun
+        ).order_by("nome").distinct("nome")
--- coredata/serializers.py
+++ coredata/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
-from coredata.models import Bairro, Municipio
+from coredata.models import (
+    Bairro,
+    Batalhao,
+    Delegacia,
+    Municipio,
+)
 
 
 class MunicipioSerializer(serializers.ModelSerializer):
@@ -12,4 +17,16 @@ class MunicipioSerializer(serializers.ModelSerializer):
 class BairroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bairro
+        fields = "__all__"
+
+
+class BatalhaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Batalhao
+        fields = "__all__"
+
+
+class DelegaciaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Delegacia
         fields = "__all__"
