b4bc7b9e58ca9000e47d14f33309c655efc4b9aa
rhenanbartels <rhenan.bartels@gmail.com>
2021-02-19 09:02:45 -0300

    Adiciona tabelas de bpm e delegacias

--- coredata/models.py
+++ coredata/models.py
@@ -34,3 +34,39 @@ class Bairro(models.Model):
     class Meta:
         managed = False
         db_table = '"bairros"."bairros_rj"'
+
+
+class Batalhao(models.Model):
+    id = models.IntegerField(primary_key=True, db_column="id")
+    bpm = models.CharField(max_length=50, db_column="bpm")
+    codigo_mun = models.ForeignKey(
+        Municipio,
+        db_column="codigo_mun",
+        on_delete=models.DO_NOTHING,
+    )
+    municipio = models.CharField(max_length=50, db_column="municipio")
+
+    def __str__(self):
+        return f"{self.municipio} - {self.bpm}"
+
+    class Meta:
+        managed = False
+        db_table = '"basegeo"."policia_areas_dps"'
+
+
+class Delegacia(models.Model):
+    id = models.BigIntegerField(primary_key=True, db_column="id")
+    nome = models.CharField(max_length=254, db_column="nome")
+    cod_mun = models.ForeignKey(
+        Municipio,
+        db_column="cod_mun",
+        on_delete=models.DO_NOTHING,
+    )
+    municipio = models.CharField(max_length=254, db_column="municipio")
+
+    def __str__(self):
+        return f"{self.municipio} - {self.nome}"
+
+    class Meta:
+        managed = False
+        db_table = '"basegeo_4326"."policia_delegacias"'
