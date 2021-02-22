7455a24499831f759f123bc9281b57e3cfc6c9b9
re-andrade1987 <renataandrade1987@hotmail.com>
2021-02-22 18:51:23 -0300

    Finalizando views cadastro

--- operations/views.py
+++ operations/views.py
@@ -12,6 +12,16 @@ class OperationReportView(LoginRequiredMixin, TemplateView):
         context["form_uuid"] = uuid.uuid4()
         return context
 
+# Classes que renderizam as páginas dos templates form
+class OperationInfoResultRegisterView(LoginRequiredMixin, TemplateView):
+    template_name = "operations/form_template_result_info.html"
 
+class OperationInfoView(LoginRequiredMixin, TemplateView):
+    template_name = "operations/form_template_info_operation.html"
+
+class OperationOcurrenceView(LoginRequiredMixin, TemplateView):
+    template_name = "operations/form_template_ocurrence.html"
+
+# Classes que renderizam as páginas dos do app
 class OperationListView(LoginRequiredMixin, TemplateView):
     template_name = "operations/operations_list_template.html"
