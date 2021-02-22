0f63ad9a679d11ce1e83302ad2f1071c9dd1474b
rhenanbartels <rhenan.bartels@gmail.com>
2021-02-22 11:56:06 -0300

    adicona rota e template para lista de operações

--- operations/urls.py
+++ operations/urls.py
@@ -5,5 +5,5 @@ from operations import views
 app_name = "operations"
 urlpatterns = [
     path("cadastro/", views.OperationReportView.as_view(), name="form"),
+    path("lista/", views.OperationListView.as_view(), name="operations-list"),
 ]
-
