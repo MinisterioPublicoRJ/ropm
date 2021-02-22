e3dd69b1512cdbc3988fb993d9c1adee419b2f55
re-andrade1987 <renataandrade1987@hotmail.com>
2021-02-22 10:41:36 -0300

    Creating pages and forms

--- operations/views.py
+++ operations/views.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 
 
 class OperationReportView(TemplateView):
-    template_name = "operations/form_template.html"
+    template_name = "operations/forms/form_template.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
