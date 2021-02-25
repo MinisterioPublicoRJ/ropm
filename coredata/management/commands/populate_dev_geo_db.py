37602a50fc6f216934802cda1e3ccca2d0da1a14
rhenanbartels <rhenan.bartels@gmail.com>
2021-02-25 08:17:03 -0300

    Aprimora script de preenchimento de dados geográficos

--- coredata/management/commands/populate_dev_geo_db.py
+++ coredata/management/commands/populate_dev_geo_db.py
@@ -1,16 +1,14 @@
-from django.apps import apps
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import connections
 
-
-app_label = "coredata"
-models = [_ for _ in apps.get_app_config(app_label).get_models()]
+from coredata.models import Bairro, Batalhao, Municipio
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        models = [Bairro, Batalhao, Municipio]
         geo_db = settings.GEO_DATABASE_NAME
         for model in models:
             schema_name = model._meta.db_table.split(".")[0]
@@ -20,3 +18,33 @@ class Command(BaseCommand):
                 with connections[geo_db].cursor() as cursor:
                     cursor.execute(f"CREATE SCHEMA IF NOT EXISTS {schema_name};")
                     cursor.execute(sql)
+
+        mun, _ = Municipio.objects.using(geo_db).get_or_create(
+            nm_mun="RIO DE JANEIRO",
+            cod_6_dig=33045,
+            cod_mun="330457",
+        )
+
+        Bairro.objects.using(geo_db).get_or_create(
+            cod_mun_id=mun.cod_6_dig,
+            bairro="CAMPO DOS AFONSOS",
+            bairro_id="rio de janeiro158",
+        )
+        Bairro.objects.using(geo_db).get_or_create(
+            cod_mun_id=mun.cod_6_dig,
+            bairro="DEODORO",
+            bairro_id="rio de janeiro156",
+        )
+
+        Batalhao.objects.using(geo_db).get_or_create(
+            codigo_mun_id=mun.cod_6_dig,
+            bpm="41º BPM",
+            id=113,
+            municipio="Rio de Janeiro"
+        )
+        Batalhao.objects.using(geo_db).get_or_create(
+            codigo_mun_id=mun.cod_6_dig,
+            bpm="9º BPM",
+            id=96,
+            municipio="Rio de Janeiro"
+        )
