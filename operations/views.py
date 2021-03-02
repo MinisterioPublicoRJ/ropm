f9810340a21fcb13109dc20bd186e558f48ee101
rhenanbartels <rhenan.bartels@gmail.com>
2021-03-02 09:52:20 -0300

    Reordena lista de operações

--- operations/views.py
+++ operations/views.py
@@ -105,7 +105,7 @@ class OperationListView(LoginRequiredMixin, ListView):
     paginate_by = settings.OPERATIONS_PER_PAGE
 
     def get_queryset(self):
-        return Operacao.objects.filter(usuario=self.request.user)
+        return Operacao.objects.filter(usuario=self.request.user).order_by("-criado_em")
 
 
 class InitialPageListView(LoginRequiredMixin, TemplateView):
