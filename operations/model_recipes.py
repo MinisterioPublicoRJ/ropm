65e7f9bf8207f0553de572fd21aa34f4cd6c8dbd
rhenanbartels <rhenan.bartels@gmail.com>
2021-03-04 09:12:32 -0300

    Corrige fixtures de testes

--- operations/model_recipes.py
+++ operations/model_recipes.py
@@ -6,6 +6,11 @@ from operations.models import Operacao
 op_recipe_with_occurence = Recipe(
     Operacao,
     houve_ocorrencia_operacao=None,
+    rg_pm_comandante_operacao="12345",
+    rg_pm_comandante_ocorrencia="67890",
+    registro_ocorrencia="034-00001/2019",
+    tipo_operacao="Pl",
+    numero_ordem_operacoes="123456",
     _fill_optional=True
 )
 
