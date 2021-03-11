5e8ec7225bb99dcd13a7fd2ade7585bf93ad2fdd
rhenanbartels <rhenan.bartels@gmail.com>
2021-03-11 15:05:36 -0300

    Adiciona informação ao list_display do admin

--- operations/admin.py
+++ operations/admin.py
@@ -5,6 +5,16 @@ from operations.models import Operacao
 
 class CustomOperacaoAdmin(admin.ModelAdmin):
     exclude = ("secao_atual", "coordenadas_geo",)
+    list_display = (
+        "batalhao_responsavel",
+        "data",
+        "municipio",
+        "bairro",
+        "localidade",
+        "tipo_operacao",
+        "criado_em",
+    )
+    ordering = ("-criado_em", "-data",)
 
     def has_change_permission(self, request, obj=None):
         return False
